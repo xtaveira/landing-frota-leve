cbec918a4eaaa7800b83e727a841afa55dedee17
Davi Taveira <davitaveira@Davis-MacBook-Air.local>
2024-10-03 19:01:09 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 
 import HeroBanner from "./components/sections/HeroBanner";
 import FeaturedMedia from "./components/sections/FeaturedMedia";
-import BenefitsList from "./components/sections/BenefitsList";
-import TestimonialsCarousel from "./components/sections/TestimonialsCarousel";
-import ProductDetails from "./components/sections/ProductDetails";
-import SecondaryCTA from "./components/sections/SecondaryCTA";
-import FAQAccordion from "./components/sections/FAQAccordion";
-import TrustBadges from "./components/sections/TrustBadges";
 import Sorteio from "./components/sections/Sorteio";
-import Footer from "./components/sections/Footer";
 import Header from "./components/sections/Header";
 
 export default function Home() {
@@ -18,13 +11,6 @@ export default function Home() {
       <Header />
       <HeroBanner />
       <FeaturedMedia />
-      {/* <BenefitsList />
-      <TestimonialsCarousel />
-      <ProductDetails />
-      <SecondaryCTA />
-      <FAQAccordion />
-      <TrustBadges />
-      <Footer /> */}
       <Sorteio />
     </div>
   );
